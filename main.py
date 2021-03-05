b44504d5d4307fff627195518d51433b18927cd4
Aayush Sureshchander <aayushs@Aayushs-MacBook-Pro.local>
2021-03-04 19:51:16 -0500

    Initial Code

---
+++ main.py
@@ -0,0 +1,6 @@
+import model_code.point_net as point_net
+
+pn = point_net.PointNet()
+
+pn.initialize_layers()
+
---
+++ model_code/point_net.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import numpy as np
+
+## This file will contain the primary class definitions for the Point Net
+
+class PointNet:
+    def __init__(self):
+        pass
+
+    def initialize_layers(self):
+        print("Layers Initialized")
+        pass
+
